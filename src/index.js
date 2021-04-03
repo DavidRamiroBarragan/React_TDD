e83c8285d926f5417b67eb851327cfcf43c67983
M. David Ramiro <dramirobarragan@gmail.com>
2021-04-03 18:44:36 +0200

    modified configurations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { AppointmentDayView } from "./Appointment";
-import { sampleAppointments } from "./sampleData";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppointmentDayView } from './AppointmentDayView';
+import { sampleAppointments } from './sampleData';
 
 ReactDOM.render(
   <AppointmentDayView appointments={sampleAppointments} />,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
     ],
   },
